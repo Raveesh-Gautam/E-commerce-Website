3e380711052b5619961b8b2b74d86d4cdbf9f222
Raveesh-Gautam <raveeshgautamfzd2002@gmail.com>
2023-12-26 02:40:36 +0530

    initial commit

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+    return (
+        <div className='descriptionbox'>
+            <div className="descriptionbox-navigator">
+                <div className="descriptionbox-nav-box">Description</div>
+                <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+
+            </div>
+            <div className="descriptionbox-description">
+                <p>Discover fashion at its finest with our curated collection of chic and trendy apparel. Elevate your 
+                    style with premium fabrics, impeccable craftsmanship, and on-trend designs. From casual essentials to
+                     glamorous ensembles, our diverse range ensures you'll find the perfect outfit for any occasion. Shop now for an unparalleled shopping experience!</p>
+           <p></p>
+            </div>
+        </div>
+    )
+}
+
+export default DescriptionBox
